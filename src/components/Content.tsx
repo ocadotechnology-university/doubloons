d223f85ebf1ae566fa75a2379a785d9c27103cd6
Andrzej <andrzej.kwasniewskiwroc@gmail.com>
2023-04-24 18:02:42 +0200

    navbar

---
+++ src/components/Content.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./Content.css"
+
+function Content() {
+    return (
+        <>
+            <div className="content">
+                <div className="title">
+                    <h2>WMS Business Processes</h2>
+                    <div className="underline"></div>
+                </div>
+                <div className="panel">
+                    <div className="containers">
+                        <div className="card-one">
+                            <h4>Name Surname</h4>
+                            <p>Collaboration</p>
+                            <p>Trust</p>
+                            <p>Autonomy</p>
+                            <p>Learn Fast</p>
+                            <p>Craftmanship</p>
+                        </div>
+                        <div className="card-two">
+                            <h4>Name Surname</h4>
+                            <p>Collaboration</p>
+                            <p>Trust</p>
+                            <p>Autonomy</p>
+                            <p>Learn Fast</p>
+                            <p>Craftmanship</p>
+                        </div>
+                        <div className="card-three">
+                            <h4>Name Surname</h4>
+                            <p>Collaboration</p>
+                            <p>Trust</p>
+                            <p>Autonomy</p>
+                            <p>Learn Fast</p>
+                            <p>Craftmanship</p>
+                        </div>
+                        <div className="card-four">
+                            <h4>Name Surname</h4>
+                            <p>Collaboration</p>
+                            <p>Trust</p>
+                            <p>Autonomy</p>
+                            <p>Learn Fast</p>
+                            <p>Craftmanship</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Content;
---
+++ src/components/Timetable.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Timetable.css"
+
+function Timetable() {
+    return (
+        <>
+            <div className="timetable">
+                <div className="container">
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Timetable;
